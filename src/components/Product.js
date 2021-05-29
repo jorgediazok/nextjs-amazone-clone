43ac1336e8095221be817966a3de5facaf8d5e8f
Jorge <jorgediazok@gmail.com>
2021-05-29 18:40:19 -0300

    all data from the cards fetched and showing no styles

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { StarIcon } from '@heroicons/react/solid';
+import Currency from 'react-currency-formatter';
+import Image from 'next/image';
+
+const MAX_RATING = 5;
+const MIN_RATING = 1;
 
 const Product = ({ id, title, description, price, image, category }) => {
-  return <div></div>;
+  const [rating] = useState(
+    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+  );
+
+  const [hasPrime] = useState(Math.random() < 0.5);
+
+  return (
+    <div>
+      <p>{category}</p>
+
+      <Image src={image} height={200} width={200} objectFit="contain" />
+
+      <h4>{title}</h4>
+
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon className="h-5" />
+          ))}
+      </div>
+
+      <p>{description}</p>
+
+      <div>
+        <Currency quantity={price} currency="ARS" />
+      </div>
+
+      {hasPrime && (
+        <div>
+          <img src="https://links.papareact.com/fdw" alt="" />
+          <p>FREE Next-Day Delivery</p>
+        </div>
+      )}
+
+      <button>Add to Basket</button>
+    </div>
+  );
 };
 
 export default Product;

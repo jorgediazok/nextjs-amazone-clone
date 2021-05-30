4d0f932fa875c6c035e5132f11dcab34c536b6cc
Jorge <jorgediazok@gmail.com>
2021-05-30 01:49:19 -0300

    checkout page completed

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Header from '../components/Header';
 import Image from 'next/image';
 import { useSelector } from 'react-redux';
-import { selectItems } from '../slices/basketSlice';
+import { selectItems, selectTotal } from '../slices/basketSlice';
 import CheckoutProduct from '../components/CheckoutProduct';
+import Currency from 'react-currency-formatter';
+import { useSession } from 'next-auth/client';
 
 const checkout = () => {
   const items = useSelector(selectItems);
+  const total = useSelector(selectTotal);
+  const [session] = useSession();
 
   return (
     <div className="bg-gray-100">
@@ -41,6 +45,29 @@ const checkout = () => {
               />
             ))}
           </div>
+        </div>
+
+        {/* Right side */}
+        <div className="flex flex-col bg-white p-10 shadow-md">
+          {items.length > 0 && (
+            <>
+              <h2 className="whitespace-nowrap">
+                Subtotal ({items.length} items):
+                <span className="font-bold">
+                  <Currency quantity={total} currency="ARS" />
+                </span>
+              </h2>
+
+              <button
+                className={`button mt-2 ${
+                  !session &&
+                  'from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed'
+                }`}
+                disabled={!session}>
+                {!session ? 'Sign in to checkout' : 'Proceed to checkout'}
+              </button>
+            </>
+          )}
         </div>
       </main>
     </div>
